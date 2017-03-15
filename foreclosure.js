18245ceba2ff67d2349b7eed90e470542e818262
Dakota <bazookaharwell@gmail.com>
2017-03-14 18:43:36 -1000

    declare private var _account

--- foreclosure.js
+++ foreclosure.js
@@ -6,5 +6,12 @@ var month = 0;
 var monthsUntilEvicted;
 
 function loan() {
-
+  var _account = {
+    borrowed: 550000,
+    balance: 286000,
+    monthlyPayment: 1700,
+    defaulted: 0,
+    defaltsToForeclose: 5,
+    foreclosed: false
+  };
 }
